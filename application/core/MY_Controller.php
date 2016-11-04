2da6108e77cbfaf6d6e8dba154721986703bf4f9
Ivelin1995 <ivelinivanov1995@gmail.com>
2016-11-04 08:42:58 -0700

    Fixed project with derek's

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -26,6 +26,9 @@ class Application extends CI_Controller
 		$this->data['pagetitle'] = "Ivelin and Derek's Coffee";
 		$this->data['ci_version'] = (ENVIRONMENT === 'development') ? 'CodeIgniter Version <strong>'.CI_VERSION.'</strong>' : '';
 		$this->load->add_package_path(APPPATH . 'third_party/caboose/');
+		// get the user role
+		$this->data['userrole'] = $this->session->userdata('userrole');
+		if ($this->data['userrole'] == NULL) $this->data['userrole'] = '?';
 	}
 
 	/**
@@ -39,9 +42,9 @@ class Application extends CI_Controller
 	 			$this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
 
 			// integrate any needed CSS framework & components
-		//$this->data['caboose_styles'] = $this->caboose->styles();
-		//$this->data['caboose_scripts'] = $this->caboose->scripts();
-		//$this->data['caboose_trailings'] = $this->caboose->trailings();
+		$this->data['caboose_styles'] = $this->caboose->styles();
+		$this->data['caboose_scripts'] = $this->caboose->scripts();
+		$this->data['caboose_trailings'] = $this->caboose->trailings();
 
  		$this->parser->parse($template, $this->data);
  	}
